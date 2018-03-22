ab35c8bb8566eeb89897dea137ac103d6aa0e4f6
Calvin <calvin.cheng@stonybrook.edu>
2018-03-22 16:23:08 -0400

    mod4.3.2

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -9,6 +9,18 @@ describe("routes : static", () => {
     it("should return status code 200", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toBe("Welcome to Bloccit");
+        done();
+      });
+    });
+
+  });
+
+  describe("GET /marco", () => {
+    it("should return status code 200 and response polo", (done) => {
+      request.get(base+'marco', (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toBe("polo");
         done();
       });
     });
