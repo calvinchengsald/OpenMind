188b9a34cdeaf85d589f3c67576ccd73f2fecc32
Calvin <calvin.cheng@stonybrook.edu>
2018-03-23 09:55:26 -0400

    mod4.3.4 bout us test fix

--- src/controllers/aboutController.js
+++ src/controllers/aboutController.js
@@ -1,6 +1,6 @@
 
 module.exports = {
   index(req, res, next){
-    res.render("static/index", {title: "About Us"});
+    res.render("static/index", {title: "About Us", messege: "About Us Page"});
   }
 }
